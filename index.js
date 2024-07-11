3e233d431fd9e3ec8bba93f851bbbe73a3c4da0a
dawine <81314834+Dawine25@users.noreply.github.com>
2024-07-11 12:29:10 +0100

    Initial Commit

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import express, { json } from 'express';
-import { initializeApp, credential as _credential, messaging } from 'firebase-admin';
-import serviceAccount from './config/getymoney-59ce1-firebase-adminsdk-lyijq-33cd6dfb6e.json';
+import express from 'express';
+import firebaseAdmin from 'firebase-admin';
+import serviceAccount from './config/getymoney-59ce1-firebase-adminsdk-lyijq-33cd6dfb6e.json' assert { type: 'json' };
 
-initializeApp({
-  credential: _credential.cert(serviceAccount)
+// Initialisez l'application Firebase
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount)
 });
 
 const app = express();
-app.use(json());
+app.use(express.json());
 
-app.post('/send-notification', (req, res) => {
+app.post('/send-notification', async (req, res) => {
   const { token, title, body } = req.body;
 
   const message = {
@@ -20,18 +21,18 @@ app.post('/send-notification', (req, res) => {
     token: token,
   };
 
-  messaging().send(message)
-    .then((response) => {
-      console.log('Successfully sent message:', response);
-      res.status(200).send({ success: true });
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error);
-      res.status(500).send({ success: false, error: error.message });
-    });
+  try {
+    const response = await firebaseAdmin.messaging().send(message);
+    console.log('Successfully sent message:', response);
+    res.status(200).send({ success: true });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).send({ success: false, error: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
