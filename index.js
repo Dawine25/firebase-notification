22dbfe48e5220786e37d28f78887f9e5bf8d52cf
dawine <81314834+Dawine25@users.noreply.github.com>
2024-07-11 16:22:06 +0100

    test

--- index.js
+++ index.js
@@ -54,57 +54,36 @@
 
 import { promises as fs } from 'fs';
 import path from 'path';
+import admin from 'firebase-admin';
 
 export default async function handler(req, res) {
   try {
-    const serviceAccountKeyPath = process.env.SERVICE_ACCOUNT_KEY_PATH;
-
-    if (!serviceAccountKeyPath) {
-      throw new Error('SERVICE_ACCOUNT_KEY_PATH is not defined');
-    }
-
-    const filePath = path.resolve(serviceAccountKeyPath);
+    const filePath = path.resolve(process.cwd(), 'config/getymoney-59ce1-944760c5f21d.json');
     const fileContent = await fs.readFile(filePath, 'utf-8');
     const serviceAccount = JSON.parse(fileContent);
 
-    // Votre logique pour envoyer une notification
-    // ...
-    firebaseAdmin.initializeApp({
-      credential: firebaseAdmin.credential.cert(serviceAccount)
-    });
-    
-    const app = express();
-    app.use(express.json());
-    //dj/
-    app.post('/send-notification', async (req, res) => {
-      const { token, title, body } = req.body;
-    
-      const message = {
-        notification: {
-          title: title,
-          body: body,
-        },
-        token: token,
-      };
-    
-      try {
-        const response = await firebaseAdmin.messaging().send(message);
-        console.log('Successfully sent message:', response);
-        res.status(200).send({ success: true });
-      } catch (error) {
-        console.error('Error sending message:', error);
-        res.status(500).send({ success: false, error: error.message });
-      }
-    });
-    
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    }
 
-    res.status(200).json({ message: 'Notification sent successfully' });
+    const { token, title, body } = req.body;
+
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      token: token,
+    };
+
+    const response = await admin.messaging().send(message);
+
+    res.status(200).json({ message: 'Notification sent successfully', response: response });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 }
+
