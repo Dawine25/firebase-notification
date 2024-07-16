5b0b1e833d306203528c7542a41440c6423a05c3
dawine <81314834+Dawine25@users.noreply.github.com>
2024-07-16 08:58:32 +0100

    deployer

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 import firebaseAdmin from 'firebase-admin';
 import express from 'express';
-import { config } from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { createRequire } from 'module';
-
-// Créer une fonction require pour charger des modules CommonJS
-const require = createRequire(import.meta.url);
-
-// Charger les variables d'environnement à partir de .env
-config({ path: fileURLToPath(new URL('.env', import.meta.url)) });
-
-// Maintenant, vous pouvez utiliser process.env pour accéder aux variables d'environnement
-const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY_PATH);
-
-
-// import serviceAccount from process.env.SERVICE_ACCOUNT_KEY_PATH assert { type: 'json' };
-
+import dotenv  from 'dotenv';
+import bodyParser from 'body-parser';
+import { readFile } from 'fs/promises';
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+const serviceAccountPath = process.env.SERVICE_ACCOUNT_KEY_PATH;
+const serviceAccount = JSON.parse(
+  await readFile(new URL(serviceAccountPath, import.meta.url))
+);
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount)
 });
 
-const app = express();
-app.use(express.json());
+
 //dj/
 app.post('/send-notification', async (req, res) => {
   const { token, title, body } = req.body;
@@ -43,6 +36,60 @@ app.post('/send-notification', async (req, res) => {
   } catch (error) {
     console.error('Error sending message:', error);
     res.status(500).send({ success: false, error: error.message });
+  }
+});
+
+app.post('/message', async (req, res) => {
+  const { token, title, body , data } = req.body;
+
+  const message = {
+    notification: {
+      title: title,
+      body: body,
+    },
+    data: data,
+    token: token,
+  };
+
+  try {
+    const response = await firebaseAdmin.messaging().send(message);
+    console.log('Successfully sent message:', response);
+    res.status(200).send({ success: true });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).send({ success: false, error: error.message });
+  }
+});
+
+app.post('/disableUser', async (req, res) => {
+  const uid = req.body.uid;
+  try {
+    await firebaseAdmin.auth().updateUser(uid, { disabled: true });
+    res.status(200).send('Utilisateur désactivé avec succès');
+  } catch (error) {
+    res.status(500).send(`Erreur lors de la désactivation de l'utilisateur: ${error}`);
+  }
+});
+
+
+app.post('/activeUser', async (req, res) => {
+  const uid = req.body.uid;
+  try {
+    await firebaseAdmin.auth().updateUser(uid, { disabled: false });
+    res.status(200).send('Utilisateur activé avec succès');
+  } catch (error) {
+    res.status(500).send(`Erreur lors de la désactivation de l'utilisateur: ${error}`);
+  }
+});
+
+
+app.post('/deleteUser', async (req, res) => {
+  const uid = req.body.uid;
+  try {
+    await firebaseAdmin.auth().deleteUser(uid);
+    res.status(200).send('Utilisateur supprimé avec succès');
+  } catch (error) {
+    res.status(500).send(`Erreur lors de la suppression de l'utilisateur: ${error}`);
   }
 });
 
